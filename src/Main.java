c6be283f0488e7f6a2562e2819c4664b62f89773
unknown <alexandruiosif187@gmail.com>
2022-03-28 21:07:01 +0300

    curs1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
+    public static void fizzbuzz(){
+        for(int i=1;i<101;i++)
+            if (i%7==0)
+                System.out.print("Rizz, ");
+            else if (i%11==0)
+                System.out.print("Jazz, ");
+            else if (i%3==0)
+                if (i%5==0)
+                    System.out.print("FizzBuzz, ");
+                else
+                    System.out.print("Fizz, ");
+            else if (i%5==0)
+                System.out.print("Buzz, ");
+            else
+                System.out.print(i+", ");
+    }
     public static void main(String[] args) {
-        System.out.println("primul curs de java google");
+     //   fizzbuzz()
     }
 }
+//fizz 3
+//buzz 5
+//fizzbuzz 3 5
+//rizz 7
+//jazz 11
+
